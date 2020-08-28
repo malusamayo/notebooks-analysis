5b5a62d04cca5f7d5eec18689ee6f10bae93ea5b
malusamayo <46087581+malusamayo@users.noreply.github.com>
2020-08-28 22:47:07 +0800

    [fix] python parser bugs of lambda expr

---
+++ Untitled-1.py
@@ -0,0 +1,43 @@
+import inspect
+import collections
+import functools
+
+funcs = collections.defaultdict(lambda: collections.defaultdict(list))
+
+
+def func_info_saver(line):
+    def inner_decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            name = func.__name__ + "_" + str(id(func))
+            args_name = tuple(inspect.signature(func).parameters)
+            arg_dict = dict(zip(args_name, args))
+            arg_dict.update(kwargs)
+            funcs[name]["loc"] = line
+            if len(funcs[name]["args"]) < 5:
+                funcs[name]["args"].append(arg_dict)
+            rets = func(*args, **kwargs)
+            if len(funcs[name]["rets"]) < 5:
+                funcs[name]["rets"].append(rets)
+
+        return wrapper
+
+    return inner_decorator
+
+
+@func_info_saver(25)
+def say_whee(x, y, z=3):
+    print("Whee!" + x + y * z)
+
+
+say_whee("Me", "hh", z=4)
+import pandas as pd
+df = pd.DataFrame([[1, 2], [3, 4]])
+f = func_info_saver(36)(lambda x: x * 2)
+df[0].map(f)
+
+for x, y in funcs.items():
+    print(x)
+    for s, t in y.items():
+        print(s, t)
+print(dict(funcs))
