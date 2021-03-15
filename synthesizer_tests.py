cbe282733628ecfa86682b9e1ff759107da20361
malusamayo <773386@gmail.com>
2021-03-15 12:49:48 +0800

    new tests

--- synthesizer_tests.py
+++ synthesizer_tests.py
@@ -11,16 +11,19 @@ def test_case1():
     df2['Sex'] = df2['Sex'].map({'male':0, 'female':1})
     checker = PatternSynthesizer(df1, df2, Info(None, None))
     l = checker.check(df1, df2)
-    print("df1", "->","df2", "\033[96m", l, "\033[0m")
+    print("df1", "->","df2", "\033[96m", checker.summary, "\033[0m")
 
 def test_case2():
     df2 = df1.copy()
     df2.drop(['Sex', 'Name'], axis=1)
     df2['Age'].fillna(0, inplace=True)
-    df2['Age'] = df2['Age'].map(lambda x: x + 1)
+    print(id(df2.index), id(df2['Age'].index))
+    # df2['Age'] = df2['Age'].map(lambda x: x + 1)
+    df2['Age'] = df2['Age'].astype(str)
+    df2.Survived = df2.Survived.astype(int)
     checker = PatternSynthesizer(df1, df2, Info(None, None))
     l = checker.check(df1, df2)
-    print("df1", "->","df2", "\033[96m", l, "\033[0m")
+    print("df1", "->","df2", "\033[96m", checker.summary, "\033[0m")
 
 def test_io():
     df2 = df1
