b293e703fcd1c15dcc04b6f69a0fbcd01bc5cfeb
malusamayo <773386@gmail.com>
2021-03-24 16:35:27 +0800

    update run script

--- run.py
+++ run.py
@@ -5,11 +5,19 @@ import subprocess
 import argparse
 import shutil
 
+CLEANCODE = '''import os, sys, matplotlib
+sys.stdout = open(os.devnull, \"w\")
+matplotlib.use('Agg')
+import warnings
+warnings.simplefilter(action='ignore', category=Warning)
+'''
+
 parser = argparse.ArgumentParser(description='Generate documentation for notebooks')
 parser.add_argument('notebook', help='the notebook to be analyzed')
 parser.add_argument('-c', '--clean', help='clean intermediate files', action="store_true")
 parser.add_argument('-s', '--skip', help='skip execution of original script', action="store_true")
 parser.add_argument('-k', '--keep', help='keep output of scripts', action="store_true")
+parser.add_argument('-i', '--ignore', help='ignore rendering html', action="store_true")
 
 args = parser.parse_args()
 
@@ -25,34 +33,34 @@ log = open(os.path.join(path, "log.txt"), "a")
 
 t = [0, 0, 0, 0]
 
-def my_print(msg):
+def print_blue(msg):
     print("\033[96m {}\033[00m".format(msg))
+
+def print_red(msg):
+    print("\033[91m {}\033[00m".format(msg))
 
 def convert():
     result = subprocess.run(["jupyter", "nbconvert", "--to", "python", args.notebook]) 
     if result.returncode:
-        print("\033[91m {}\033[00m".format("Notebook conversion failed!"))
+        print_red("Notebook conversion failed!")
         log.write(filename + "\t" + "Notebook conversion failed\n")
         log.close()
         sys.exit(-5)
     result = subprocess.run(["jupyter", "nbconvert", "--to", "html", args.notebook]) 
     if result.returncode:
-        print("\033[91m {}\033[00m".format("Notebook conversion failed!"))
+        print_red("Notebook conversion failed!")
         log.write(filename + "\t" + "Notebook conversion failed\n")
         log.close()
         sys.exit(-5)
 
 def execute_script():
-    if args.skip:
-        return
-    os.chdir(path)
-    my_print('-'*40)
-    my_print("Cleaning the original notebook...")
+    print_blue('-'*40)
+    print_blue("Cleaning the original notebook...")
     # clean up the python code first
-    with open(filename_no_suffix + ".py", "r+", encoding='utf-8') as f:
+    with open(path + filename_no_suffix + ".py", "r+", encoding='utf-8') as f:
         content = ""
         if not args.keep:
-            content += "import os, sys, matplotlib \nsys.stdout = open(os.devnull, \"w\")\nmatplotlib.use('Agg')\n"
+            content += CLEANCODE
         for line in f:
             if line.startswith("get_ipython") or line.startswith("display("):
                 line = "# " + line
@@ -60,103 +68,110 @@ def execute_script():
         f.seek(0)
         f.truncate()
         f.write(content)
+    
+    if args.skip:
+        return
 
-    my_print("Running the original notebook...")
+    os.chdir(path)
+    print_blue("Running the original notebook...")
     st = time.time()
     result = subprocess.run(["python", filename_no_suffix + ".py"])
     ed0 = time.time()
     t[0] = ed0 - st
     os.chdir(owd)
     if result.returncode:
-        print("\033[91m {}\033[00m".format("Original execution failed!"))
+        print_red("Original execution failed!")
         log.write(filename + "\t" + "Script failed\n")
         log.close()
         sys.exit(-1)
-    print("\033[96m {}\033[00m".format(
-        "Original execution completed in {:.2f} seconds.".format(t[0])))
+    print_blue("Original execution completed in {:.2f} seconds.".format(t[0]))
 
 def static_analysis():
-    print("\033[96m {}\033[00m".format('-'*40))
-    print("\033[96m {}\033[00m".format(
-        "Starting static analysis of the notebook..."))
+    print_blue('-'*40)
+    print_blue("Starting static analysis of the notebook...")
     st = time.time()
     result = subprocess.run(
         ["node", os.path.join(SRC_PATH, "instrumenter.js"), path + filename_no_suffix + ".py"])
     ed1 = time.time()
     t[1] = ed1 - st
     if result.returncode:
-        print("\033[91m {}\033[00m".format("Static analysis failed!"))
+        print_red("Static analysis failed!")
         log.write(filename + "\t" + "Static analysis failed\n")
         log.close()
         sys.exit(-2)
-    print("\033[96m {}\033[00m".format(
-        "Static analysis completed in {:.2f} seconds.".format(t[1])))
+    print_blue("Static analysis completed in {:.2f} seconds.".format(t[1]))
 
 def dynamic_analysis():
     os.chdir(path)
-    print("\033[96m {}\033[00m".format('-'*40))
-    print("\033[96m {}\033[00m".format(
-        "Starting dynamic analysis of the notebook..."))
+    print_blue('-'*40)
+    print_blue("Starting dynamic analysis of the notebook...")
     st = time.time()
     result = subprocess.run(["python", filename_no_suffix + "_m.py"])
     ed2 = time.time()
     t[2] = ed2 - st
     os.chdir(owd)
     if result.returncode:
-        print("\033[91m {}\033[00m".format("Dynamic analysis failed!"))
+        print_red("Dynamic analysis failed!")
         log.write(filename + "\t" + "Dynamic analysis failed\n")
         log.close()
         sys.exit(-3)
-    print("\033[96m {}\033[00m".format(
-        "Dynamic analysis completed in {:.2f} seconds.".format(t[2])))
+    print_blue("Dynamic analysis completed in {:.2f} seconds.".format(t[2]))
 
 def analyze():
-    print("\033[96m {}\033[00m".format('-'*40))
-    print("\033[96m {}\033[00m".format("Starting generating documentation..."))
+    print_blue('-'*40)
+    print_blue("Starting generating documentation...")
     st = time.time()
     result = subprocess.run(["python", os.path.join(SRC_PATH, "analyzer.py"), args.notebook])
     ed3 = time.time()
     t[3] = ed3 - st
     if result.returncode:
-        print("\033[91m {}\033[00m".format("Doc gen failed!"))
+        print_red("Doc gen failed!")
         log.write(filename + "\t" + "Doc gen failed\n")
         log.close()
         sys.exit(-4)
-    print("\033[96m {}\033[00m".format(
+    print_blue(
     '''Generation completed in {:.2f} seconds.
     Script completed in {:.2f} seconds.
     Time fraction: [{:.2f} vs {:.2f}, {:.2f}, {:.2f}] seconds
     '''
-    .format(t[3], t[1]+t[2]+t[3], t[0], t[1], t[2], t[3])))
+    .format(t[3], t[1]+t[2]+t[3], t[0], t[1], t[2], t[3]))
     log.write(filename + "\t" + "{:.2f}\t{:.2f}\t{:.2f}\t{:.2f}\t{:.2f}\n".format(t[0], t[1], t[2], t[3], t[1]+t[2]+t[3]))
     log.close()
 
 def render():
-    print("\033[96m {}\033[00m".format('-'*40))
-    print("\033[96m {}\033[00m".format("Converting documentation to html..."))
-    result = subprocess.run(["node", os.path.join(SRC_PATH, "html_convert.js"), args.notebook])
-    if result.returncode:
+    if args.ignore:
         return
+    print_blue('-'*40)
+    print_blue("Converting documentation to html...")
+    st = time.time()
+    result = subprocess.run(["node", os.path.join(SRC_PATH, "html_convert.js"), args.notebook])
+    ed4 = time.time()
+    if result.returncode:
+        print_red("HTML convert failed!")
+        return
+    print_blue("HTML rendering completed in {:.2f} seconds.".format(ed4 - st))
 
 def clean():
     try:
         shutil.rmtree(os.path.join(path, filename_no_suffix))
-        my_print("removing folders")
+        print_blue("removing folders")
     except FileNotFoundError:
-        my_print("no previous folder")
+        print_blue("no previous folder")
     # os.system("rm " + os.path.join(path, filename_no_suffix, "\*.json"))
     # os.system("rm " + os.path.join(path, filename_no_suffix, ".json"))
     # subprocess.run(["rmdir", os.path.join(path, filename_no_suffix)])
     if not args.clean:
         return
+    subprocess.run(["rm", os.path.join(path, filename_no_suffix + ".html")])
     subprocess.run(["rm", os.path.join(path, filename_no_suffix + ".py")])
     subprocess.run(["rm", os.path.join(path, filename_no_suffix + "_m.py")])
     subprocess.run(["rm", os.path.join(path, filename_no_suffix + "_comment.json")])
 
-clean()
-convert()
-execute_script()
-static_analysis()
-dynamic_analysis()
-analyze()
-render()
+if __name__ == "__main__":
+    clean()
+    convert()
+    execute_script()
+    static_analysis()
+    dynamic_analysis()
+    analyze()
+    render()
