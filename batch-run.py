35440f740b9c26cfe0574140961254148ddf5c93
Ubuntu <ubuntu@ip-172-26-2-195.ap-northeast-2.compute.internal>
2021-03-25 09:34:39 +0000

    sorted batch

--- batch-run.py
+++ batch-run.py
@@ -4,7 +4,7 @@ def my_print(msg):
     print("\033[96m {}\033[00m".format(msg))
 
 dir = sys.argv[1]
-for file in os.listdir(dir):
+for file in sorted(os.listdir(dir)):
     if file.endswith(".ipynb"):
         file_path = os.path.join(dir, file)
         my_print("Running " + file_path)
