a691d3a1b1e2321b213d250a0a17300b156901e7
malusamayo <773386@gmail.com>
2021-03-29 17:26:05 +0800

    update options

--- batch-run.py
+++ batch-run.py
@@ -4,8 +4,9 @@ def my_print(msg):
     print("\033[96m {}\033[00m".format(msg))
 
 dir = sys.argv[1]
+option = sys.argv[2]
 for file in sorted(os.listdir(dir)):
     if file.endswith(".ipynb"):
         file_path = os.path.join(dir, file)
         my_print("Running " + file_path)
-        os.system("python run.py " + file_path)
+        os.system(" ".join(["python", "run.py", file_path, option]))
