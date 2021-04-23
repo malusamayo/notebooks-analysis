56446952fd2fcc5267f4d7a13bdb78be30ba6026
Ubuntu <ubuntu@ip-172-26-2-195.ap-northeast-2.compute.internal>
2021-04-23 14:07:35 +0000

    update script

--- notebooks_analysis/log_analysis.py
+++ notebooks_analysis/log_analysis.py
@@ -1,23 +1,41 @@
 import os, sys
+from statistics import median
+from collections import defaultdict, ChainMap
 
-info = {}
+info = defaultdict(dict)
 
-with open(sys.argv[1], "r") as f:
-    for line in f:
-        ls = line[:-1].split("\t")
-        if ls[-1] == "success":
-            ls = ls[:-1]
-            info[ls[0]] = [float(x)/float(ls[1]) for x in ls[1:]]
+files = [
+    os.path.join("notebooks", "titanic", "log.txt"), 
+    os.path.join("notebooks", "fifa19", "log.txt"),
+    os.path.join("notebooks", "google", "log.txt"), 
+    os.path.join("notebooks", "airbnb", "log.txt")
+]
 
+for file_name in files:
+    with open(file_name, "r") as f:
+        for line in f:
+            ls = line[:-1].split("\t")
+            if ls[-1] == "success":
+                ls = ls[:-1]
+                info[file_name][ls[0]] = [float(x)/float(ls[1]) for x in ls[1:]] + [0] + [float(ls[i]) for i in range(1, len(ls))]
 
-    
-data = list(info.values())
-avg = [float(sum(col))/len(col) for col in zip(*data)]
+def print_data(cur_dict):
+    data = list(cur_dict.values())
+    avg = [float(sum(col))/len(col) for col in zip(*data)]
+    med = [median(col) for col in zip(*data)]
 
-track_idx = 2
+    track_idx = 2
 
-for k, v in sorted(info.items(), key=lambda item: item[1][track_idx]):
-    if v[track_idx] > avg[track_idx]:
-        print(k, v)
+    # for k, v in sorted(cur_dict.items(), key=lambda item: item[1][track_idx]):
+    #     if v[track_idx] > avg[track_idx]:
+    #         print(k, v)
 
-print(avg)
+    print(avg, avg[-1]/avg[-2])
+    # print(med)
+
+for file_name in files:
+    cur_dict = info[file_name]
+    print(file_name)
+    print_data(cur_dict)
+
+print_data(dict(ChainMap(*info.values())))
--- notebooks_analysis/synthesizer_tests.py
+++ notebooks_analysis/synthesizer_tests.py
@@ -3,6 +3,7 @@ import sys, pickle
 sys.argv.append("./notebooks/debug_example.ipynb")
 
 from analyzer import PatternSynthesizer, Info
+from model import dispatch_gen
 
 
 df1 = pd.read_csv('../notebooks/input/train.csv')
@@ -11,7 +12,7 @@ def test_case1():
     df2 = df1.copy()
     df2 = df1[~df1.isnull().any(axis=1)]
     df2['Sex'] = df2['Sex'].map({'male':0, 'female':1})
-    checker = PatternSynthesizer(df1, df2, Info(None, None))
+    checker = PatternSynthesizer(dispatch_gen(df1, "df1", 0, 0), dispatch_gen(df2, "df2", 0, 1), Info(None, None), [])
     l = checker.check(df1, df2)
     print("df1", "->","df2", "\033[96m", checker.summary, "\033[0m")
 
@@ -23,7 +24,7 @@ def test_case2():
     # df2['Age'] = df2['Age'].map(lambda x: x + 1)
     df2['Age'] = df2['Age'].astype(str)
     df2.Survived = df2.Survived.astype(int)
-    checker = PatternSynthesizer(df1, df2, Info(None, None))
+    checker = PatternSynthesizer(dispatch_gen(df1, "df1", 0, 0), dispatch_gen(df2, "df2", 0, 1), Info(None, None), [])
     l = checker.check(df1, df2)
     print("df1", "->","df2", "\033[96m", checker.summary, "\033[0m")
 
@@ -34,21 +35,31 @@ def test_io():
     with open("test.dat", "rb") as f:
         vars = pickle.load(f)
         df1, df2 = vars[0], vars[1]
-        checker = PatternSynthesizer(df1, df2, Info(None, None))
+        checker = PatternSynthesizer(dispatch_gen(df1, "df1", 0, 0), dispatch_gen(df2, "df2", 0, 1), Info(None, None), [])
         l = checker.check(df1, df2)
         print("df1", "->","df2", "\033[96m", l, "\033[0m")
 
 def test_double():
     df2 = df1.copy()
     df2.dropna(inplace=True)
-    checker = PatternSynthesizer(df1, df2, Info(None, None))
+    checker = PatternSynthesizer(dispatch_gen(df1, "df1", 0, 0), dispatch_gen(df2, "df2", 0, 1), Info(None, None), [])
     l = checker.check(df1, df2)
     print("df1", "->","df2", "\033[96m", checker.summary, "\033[0m")
     l = checker.check(df1, df2)
     print("df1", "->","df2", "\033[96m", checker.summary, "\033[0m")
 
+def test_groupby():
+    df2 = df1.copy()
+    df2 = df2.groupby(['Name'])['Age'].sum()
+    df2 = df2.reset_index()
+    df2 = df2.sort_values('Age', ascending = False)
+    checker = PatternSynthesizer(dispatch_gen(df1, "df1", 0, 0), dispatch_gen(df2, "df2", 0, 1), Info(None, None), [])
+    l = checker.check(df1, df2)
+    print("df1", "->","df2", "\033[96m", checker.summary, "\033[0m")
+
+
 def run_all_tests():
-    tests = [test_case1, test_case2, test_double]
+    tests = [test_case1, test_case2, test_double, test_groupby]
     for test in tests:
         test()
 
